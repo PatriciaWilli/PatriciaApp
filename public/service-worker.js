4fc99e7fbc0a26b62575e80424245fde656c2fdc
PatriciaWilli <patricia.willi@sasis.ch>
2025-10-31 09:00:37 +0100

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,43 +1,31 @@
+// Service Worker für PWA Offline-Funktionalität
 const CACHE_NAME = 'event-manager-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/static/js/bundle.js',
-  '/static/js/main.chunk.js',
-  '/static/js/0.chunk.js',
-  '/manifest.json'
+  '/static/css/main.css',
+  '/static/js/main.js'
 ];
 
-self.addEventListener('install', event => {
+// Installation
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
+      .then((cache) => {
+        console.log('Cache geöffnet');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
+// Aktivierung
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Alten Cache löschen:', cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -45,3 +33,24 @@ self.addEventListener('activate', event => {
     })
   );
 });
+
+// Fetch - Network First Strategy
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // Erfolgreiche Antwort -> Cache aktualisieren
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        // Netzwerk-Fehler -> Aus Cache laden
+        return caches.match(event.request);
+      })
+  );
+});
+
+
